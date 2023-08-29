e4862ec9e650982f38663ba3a8946c5dabeef491
Shopping <xpzheng6@163.com>
2023-08-29 22:01:42 +0800

    feat(dom): watermark options

--- src/dom/watermark.ts
+++ src/dom/watermark.ts
@@ -1,3 +1,20 @@
-export function createWatermark() {
+export interface CreateWatermarkOptions {
+  // 字
+  fontFamily?: string
+  textColor?: string
+  textSize?: number
+  fontBold?: number
+  rotate?: number
+  // 背景
+  bgColor?: string
+  // 其他
+  gap?: number
+}
+
+export function createWatermark(options: CreateWatermarkOptions) {
   // TODO
+  const canvas = document.createElement('canvas')
+  const ctx = canvas.getContext('2d')
+
+
 }
