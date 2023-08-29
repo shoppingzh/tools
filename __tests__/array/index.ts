cd4f40b7bbe8dcb98b3088429fd6acb7c4a6da7c
Shopping <xpzheng6@163.com>
2023-08-29 22:01:24 +0800

    feat(array): insert api

--- __tests__/array/index.ts
+++ __tests__/array/index.ts
@@ -1,8 +1,27 @@
-import { isArray } from '@/array'
+import { isArray, insert } from '@/array'
 
 describe('isArray', () => {
 
   it('base', () => {
     expect(isArray([])).toBeTruthy()
   })
+
+})
+
+describe('insert', () => {
+
+  it('single', () => {
+    const arr = [1, 2, 3, 4, 5]
+    const result = insert(arr, -1, 0)
+    expect(arr[0]).toBe(0)
+    expect(result).toBe(0)
+  })
+
+  it('multiple', () => {
+    const arr = [1, 2, 3, 4, 5]
+    const result = insert(arr, -1, [-2, -1, 0])
+    expect(arr).toEqual([-2, -1, 0, 1, 2, 3, 4, 5])
+    expect(result).toEqual([-2, -1, 0])
+  })
+
 })
--- src/array/index.ts
+++ src/array/index.ts
@@ -8,3 +8,24 @@ import { getObjectType } from '@/_internal/lang'
 export function isArray(value: any) {
   return getObjectType(value) === 'Array'
 }
+
+/**
+ * 在数组特定位置的后面插入N个元素
+ * 
+ * @param arr 数组
+ * @param index 位置
+ * @param object 插入的元素
+ * @returns 
+ */
+export function insert<T>(arr: T[], index: number, objects: T[]): T[];
+export function insert<T>(arr: T[], index: number, object: T): T;
+export function insert<T>(arr: T[], index: number, object: any) {
+  if (!arr) return null
+  if (Array.isArray(object)) {
+    arr.splice(index + 1, 0, ...object)
+  } else {
+    arr.splice(index + 1, 0, object)
+  }
+
+  return object
+}
