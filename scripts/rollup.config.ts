e751cd6105edd7d4ff2b6b730271c8f1a64e0c41
shoppingzh <xpzheng6@163.com>
2023-08-29 18:39:51 +0800

    feat: generate dts file

--- scripts/rollup.config.ts
+++ scripts/rollup.config.ts
@@ -10,38 +10,54 @@ import pkg from '../package.json'
 import clear from 'rollup-plugin-clear'
 import strip from '@rollup/plugin-strip'
 import config from './config'
+import { dts } from 'rollup-plugin-dts'
 
-export default defineConfig({
-  input: config.input,
-  output: [{
-    dir: 'lib',
-    format: 'esm',
-  }],
-  external: Object.keys((pkg as any).peerDependencies || {}),
-  plugins: [
-    alias({
-      entries: {
-        '@': path.resolve(__dirname, '../src')
-      }
-    }),
-    clear({
-      targets: ['dist'],
-    }),
-    ts({
-      tsconfig: path.resolve(__dirname, '../tsconfig.json'),
-    }),
-    babel({
-      babelHelpers: 'runtime',
-    }),
-    // 去除console.log
-    strip({
-      include: 'src/**/*.{ts,js}'
-    }),
-    // 生成包大小监控
-    sizes(100),
-    // 代码混淆
-    terser(),
-    // 警告声
-    beep(),
-  ],
-})
+export default [
+  defineConfig({
+    input: config.input,
+    output: [{
+      dir: 'lib',
+      format: 'esm',
+    }],
+    external: Object.keys((pkg as any).peerDependencies || {}),
+    plugins: [
+      alias({
+        entries: {
+          '@': path.resolve(__dirname, '../src')
+        }
+      }),
+      clear({
+        targets: ['dist'],
+      }),
+      ts({
+        tsconfig: path.resolve(__dirname, '../tsconfig.json'),
+      }),
+      babel({
+        babelHelpers: 'runtime',
+      }),
+      // 去除console.log
+      strip({
+        include: 'src/**/*.{ts,js}'
+      }),
+      // 生成包大小监控
+      sizes(100),
+      // 代码混淆
+      terser(),
+      // 警告声
+      beep(),
+    ],
+  }),
+  defineConfig({
+    input: config.input,
+    output: {
+      dir: 'lib',
+    },
+    plugins: [
+      dts({
+        compilerOptions: {
+          preserveSymlinks: false,
+        }
+      })
+    ]
+  })
+]
