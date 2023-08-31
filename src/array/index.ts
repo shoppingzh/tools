6e4fcffe8c8f6daf1da7b57764bc7c1550a3070a
shoppingzh <xpzheng6@163.com>
2023-08-31 11:22:46 +0800

    feat(array): change insert implemention, update tests

--- src/array/index.ts
+++ src/array/index.ts
@@ -10,22 +10,19 @@ export function isArray(value: any) {
 }
 
 /**
- * 在数组特定位置的后面插入N个元素
+ * 在数组特定位置的前面插入N个元素
+ * 
+ * 注：当插入位置小于0时将报错，大于最大索引时，将插入到数组的最后
  * 
  * @param arr 数组
  * @param index 位置
- * @param object 插入的元素
- * @returns 
+ * @param objects 插入的元素
+ * @returns 返回原数组
  */
 export function insert<T>(arr: T[], index: number, objects: T[]): T[];
-export function insert<T>(arr: T[], index: number, object: T): T;
-export function insert<T>(arr: T[], index: number, object: any) {
-  if (!arr) return null
-  if (Array.isArray(object)) {
-    arr.splice(index + 1, 0, ...object)
-  } else {
-    arr.splice(index + 1, 0, object)
-  }
-
-  return object
+export function insert<T>(arr: T[], index: number, objects: T): T;
+export function insert<T>(arr: T[], index: number, objects: any) {
+  if (index < 0) throw new Error('out of index')
+  arr.splice(index, 0, ...(Array.isArray(objects) ? objects : [objects]))
+  return arr
 }
