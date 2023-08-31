01bc616e99ee3c6013f58f6991dcfe270c8e2643
shoppingzh <xpzheng6@163.com>
2023-08-31 14:46:04 +0800

    feat(array): remove api and tests

--- src/array/index.ts
+++ src/array/index.ts
@@ -26,3 +26,26 @@ export function insert<T>(arr: T[], index: number, objects: any) {
   arr.splice(index, 0, ...(Array.isArray(objects) ? objects : [objects]))
   return arr
 }
+
+/**
+ * 删除数组指定索引的元素
+ * 
+ * @param arr 数组
+ * @param indexes 索引
+ */
+export function remove<T>(arr: T[], indexes: number): T[];
+export function remove<T>(arr: T[], indexes: number[]): T[];
+export function remove<T>(arr: T[], indexes: any) {
+  let indexList = Array.isArray(indexes) ? indexes : [indexes]
+  // 去重并从小到大排序
+  indexList = [...new Set(indexList)].sort()
+
+  const result: T[] = []
+
+  for (let len = indexList.length, i = len - 1;i >= 0;i--) {
+    const removeList = arr.splice(indexList[i], 1)
+    result.unshift(...removeList)
+  }
+
+  return result
+}
