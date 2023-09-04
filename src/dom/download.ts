bc396ff5651ad60905d9693f7cb23d58fcea4e16
shoppingzh <xpzheng6@163.com>
2023-09-04 18:37:13 +0800

    feat(dom): download api

--- src/dom/download.ts
+++ src/dom/download.ts
@@ -1,19 +1,32 @@
-import { merge } from 'lodash'
 import { setStyle } from '.'
 
 export interface DownloadOptions {
   filename?: string
 }
 
-export function download(url: string, options?: DownloadOptions) {
-  const opts = merge({}, options)
+/**
+ * 下载
+ * 
+ * @param url 下载链接
+ * @param filename 文件名
+ */
+export function download(url: string, filename?: string, newWindow = true) {
   const el = document.createElement('a')
   setStyle(el, {
-    display: 'none'
+    display: 'none',
+    position: 'absolute',
   })
   el.href = url
-  if (opts.filename) {
-    el.download = ''
+
+  if (filename) {
+    el.download = filename
+  } else {
+    const parts = url.split('/')
+    const filenamePart = parts[parts.length - 1]
+    el.download = filenamePart.split('?')[0]
+  }
+  if (newWindow) {
+    el.target = '_blank'
   }
   el.addEventListener('click', () => {
     document.body.removeChild(el)
