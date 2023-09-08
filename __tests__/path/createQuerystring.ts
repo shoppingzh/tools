beb252a3ca0228485e0cb372ae9561a11618b16d
shoppingzh <xpzheng6@163.com>
2023-09-08 11:57:32 +0800

    feat(path): createQuerystring, strong!

--- __tests__/path/createQuerystring.ts
+++ __tests__/path/createQuerystring.ts
@@ -21,3 +21,40 @@ it('object null', () => {
 it('not object', () => {
   expect(() => createQuerystring(1 as any)).toThrow()
 })
+
+it('object value is undefined', () => {
+  expect(createQuerystring({ a: undefined, b: 1 })).toBe('b=1')
+})
+
+it('object value is null', () => {
+  expect(createQuerystring({ a: null, b: 1 })).toBe('a=&b=1')
+})
+
+it('object value is base type', () => {
+  expect(createQuerystring({
+    a: 'hello',
+    b: true,
+    c: 2,
+    d: Symbol('hello'),
+    e: BigInt(100)
+  })).toBe('a=hello&b=true&c=2&e=100')
+})
+
+it('object value is array', () => {
+  expect(createQuerystring({
+    a: [1, 2, 3, { aa: 4, ab: 5 }]
+  })).toBe('a=1&a=2&a=3&a=[object Object]')
+})
+
+it('object value is Set', () => {
+  expect(createQuerystring({
+    a: new Set([1, 2, 3])
+  })).toBe('a=1&a=2&a=3')
+})
+
+it('object value is object', () => {
+  const date = new Date(2023, 9, 8, 10, 0, 0, 0)
+  expect(createQuerystring({
+    a: date
+  })).toBe(`a=${date.toString()}`)
+})
--- src/path/createQuerystring.ts
+++ src/path/createQuerystring.ts
@@ -1,8 +1,47 @@
-import { isObject } from 'lodash'
+import { isArray, isArrayLike, isObject, isSet, isSymbol, merge } from 'lodash'
 
-export function getQueryValue(value: any): string {
-  if (value == null) return ''
-  return String(value)
+interface Options<T> {
+  encode?: boolean
+}
+interface Pair {
+  name?: string
+  value?: string
+}
+
+function getQueryValue(value: any, encode: boolean) {
+  const strValue = String(value)
+  return encode ? encodeURIComponent(strValue) : strValue
+}
+
+function getPairs<T>(name: string, value: any, options: Options<T>, deep = true): Pair[] {
+  // undefined跳过
+  if (value === undefined) return []
+  const pairs: Pair[] = []
+  const encode = options.encode
+
+  if (value === null) {
+    pairs.push({ name, value: getQueryValue('', encode) })
+  } else {
+    // 数组/Set展开
+    if (isArray(value) || isSet(value)) {
+      for (const subValue of value) {
+        if (deep) {
+          const subPairs = getPairs(name, subValue, options, false)
+          pairs.push(...subPairs)
+        } else {
+          pairs.push({ name, value: getQueryValue(subValue, encode) })
+        }
+      }
+    } else if (isSymbol(value)) {
+      // symbol跳过，处理了没意义
+    } else {
+      // 其他一律转为字符串
+      pairs.push({ name, value: getQueryValue(value, encode) })
+    }
+  }
+
+
+  return pairs
 }
 
 /**
@@ -11,21 +50,19 @@ export function getQueryValue(value: any): string {
  * 注：对象的undefined属性会跳过，null属性会被处理为空字符串
  * 
  * @param object 对象
- * @param encode 是否编码
+ * @param options 配置
  * @returns 
  */
-export function createQuerystring<T extends object>(object: T, encode?: boolean): string {
+export function createQuerystring<T extends object>(object: T, options?: Options<T>): string {
   if (object == null) return ''
   if (!isObject(object)) throw new Error('object is not Object!')
+  const opts = merge({}, options)
 
-  const pairs: string[] = []
+  const pairs: Pair[] = []
   Object.entries(object).forEach(([name, value]) => {
-    if (value === undefined) return
-    let queryValue = getQueryValue(value)
-    if (encode) {
-      queryValue = encodeURIComponent(queryValue)
-    }
-    pairs.push(`${name}=${queryValue}`)
+    const currentPairs = getPairs(name, value, opts)
+    pairs.push(...currentPairs)
   })
-  return pairs.join('&')
+
+  return pairs.map(o => `${o.name}=${o.value}`).join('&')
 }
