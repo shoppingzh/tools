d690a012d802c3426833706ebe8da586ff19581b
Shopping <xpzheng6@163.com>
2023-09-09 22:42:33 +0800

    feat(path): parseFilename, api design

--- src/path/parseFilename.ts
+++ src/path/parseFilename.ts
@@ -1,3 +1,8 @@
-export function parseFilename() {
-  // 
+interface ParseResult {
+  basename: string
+  extname: string
+}
+
+export function parseFilename(filename: string): ParseResult {
+  return null
 }
