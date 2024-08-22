747f520525ad4b5fc1db686af58d1cfda4f090a8
Shopping <xpzheng6@163.com>
2024-08-22 21:17:14 +0800

    feat: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,26 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const js = require('@shoppingzh/eslint-config/javascript')
-const ts = require('@shoppingzh/eslint-config/typescript')
-const stylistic = require('@shoppingzh/eslint-config/stylistic')
+const { javascript, stylistic, typescript, } = require('@shoppingzh/eslint-config')
 const globals = require('globals')
 
 module.exports = [
   {
-    ignores: [
-      'lib/**',
-    ],
+    ignores: ['lib']
   },
-  ...stylistic(),
-  ...js({
+  ...javascript({
     globals: {
       ...globals.jest,
-    }
+    },
   }),
-  ...ts({
+  ...stylistic(),
+  ...typescript({
     overrides: {
       '@typescript-eslint/no-explicit-any': [0],
       '@typescript-eslint/no-unused-vars': [1],
     }
   }),
+  {
+    files: ['*.js', 'performance/*.js'],
+    rules: {
+      '@typescript-eslint/no-require-imports': [0],
+    }
+  }
 ]
