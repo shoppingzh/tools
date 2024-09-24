27cf785fffc35d49af80e568753ce1069e010ab4
shoppingzh <xpzheng6@163.com>
2024-09-24 16:33:06 +0800

    feat(tree): find & flat tests

--- __tests__/tree/find.ts
+++ __tests__/tree/find.ts
@@ -11,17 +11,69 @@ it('not found', () => {
   expect(node).toBeUndefined()
 })
 
-it('bfs: find first leaf node', () => {
+it('find first leaf node bfs strategy', () => {
   const node = find(nodes, node => !node.children || !node.children.length)
   expect(node?.name).toBe('3')
 })
 
-it('dfs-pre: find first leaf node', () => {
+it('find first leaf node by dfs-pre strategy', () => {
   const node = find(nodes, node => !node.children?.length, 'dfs-pre')
   expect(node?.name).toBe('1-1-1')
 })
 
-it('dfs-post: find first leaf node', () => {
+it('find first leaf node by dfs-post strategy', () => {
   const node = find(nodes, node => !node.children?.length, 'dfs-pre')
   expect(node?.name).toBe('1-1-1')
+})
+
+it('nodes is empty', () => {
+  expect(find([], node => node.name === '1')).toBeUndefined()
+})
+
+it('parent is nil', () => {
+  expect(find(nodes, (node, parent) => parent == null)?.name).toBe('1')
+})
+
+it('depth is 2', () => {
+  expect(find(nodes, (node, parent, depth) => depth === 2)?.name).toBe('1-1-1')
+})
+
+it('depth is 7', () => {
+  expect(find(nodes, (node, parent, depth) => depth === 7)).toBeUndefined()
+})
+
+it('expection: nodes is nil', () => {
+  expect(() => find(null, () => true)).toThrow()
+})
+
+it('exception: nodes is not array', () => {
+  expect(() => find(1 as any, () => true)).toThrow()
+  expect(() => find('1' as any, () => true)).toThrow()
+  expect(() => find(true as any, () => true)).toThrow()
+  expect(() => find({} as any, () => true)).toThrow()
+  expect(() => find(Symbol.iterator as any, () => true)).toThrow()
+  expect(() => find(Math as any, () => true)).toThrow()
+})
+
+it('exception: predicate is nil', () => {
+  expect(() => find(nodes, null)).toThrow()
+  expect(() => find(nodes, undefined)).toThrow()
+})
+
+it('exception: predicate is not function', () => {
+  expect(() => find(nodes, 1 as any)).toThrow()
+  expect(() => find(nodes, '1' as any)).toThrow()
+  expect(() => find(nodes, true as any)).toThrow()
+  expect(() => find(nodes, {} as any)).toThrow()
+  expect(() => find(nodes, Symbol.iterator as any)).toThrow()
+  expect(() => find(nodes, Math as any)).toThrow()
+  expect(() => find(nodes, [] as any)).toThrow()
+})
+
+it('exception: unknown traverseStrategy', () => {
+  expect(() => find(nodes, () => true, 'unknown' as any)).toThrow()
+})
+
+it('childrenProp', () => {
+  expect(find(nodes, (node) => node.name === '1-1', 'bfs', 'childNodes' as any)).toBeUndefined()
 })
--- __tests__/tree/flat.ts
+++ __tests__/tree/flat.ts
@@ -1,29 +1,60 @@
 import { flat } from '@/tree'
 import nodes from './_nodes'
 
+const flatNames = [
+  '1-1-2',
+  '1-2-1',
+  '1-1-1',
+  '1',
+  '1-2-2-2-1',
+  '1-1',
+  '1-2',
+  '1-2-2-2-2',
+  '3',
+  '1-2-2-2',
+  '2',
+  '1-2-2',
+  '2-1',
+  '1-2-2-1',
+  '2-2',
+].sort()
+
 it('base', () => {
   const flatNodes = flat(nodes)
   expect(flatNodes.length).toBe(15)
-  expect(flatNodes.map(o => o.name).sort()).toEqual([
-    '1-1-2',
-    '1-2-1',
-    '1-1-1',
-    '1',
-    '1-2-2-2-1',
-    '1-1',
-    '1-2',
-    '1-2-2-2-2',
-    '3',
-    '1-2-2-2',
-    '2',
-    '1-2-2',
-    '2-1',
-    '1-2-2-1',
-    '2-2',
-  ].sort())
+  expect(flatNodes.map(o => o.name).sort()).toEqual(flatNames)
+})
+
+
+it('nodes is empty', () => {
+  expect(flat([])).toEqual([])
 })
 
 it('exception: nodes is nil', () => {
   expect(() => flat(undefined)).toThrow()
   expect(() => flat(null)).toThrow()
+})
+
+it('dfs-pre traverse strategy', () => {
+  expect(flat(nodes, 'dfs-pre').map(o => o.name).sort()).toEqual(flatNames)
+})
+
+it('dfs-post traverse strategy', () => {
+  expect(flat(nodes, 'dfs-post').map(o => o.name).sort()).toEqual(flatNames)
+})
+
+it('bfs traverse strategy', () => {
+  expect(flat(nodes, 'bfs').map(o => o.name).sort()).toEqual(flatNames)
+})
+
+it('exception: unknown traverse strategy', () => {
+  expect(() => flat(nodes, 'unknown' as any)).toThrow()
+})
+
+it('exception: traverse strategy is nil', () => {
+  expect(() => flat(nodes, null)).toThrow()
+})
+
+it('childrenProp', () => {
+  expect(flat(nodes, 'bfs', 'childNodes' as any).map(o => o.name)).toEqual(['1', '2', '3'])
 })
