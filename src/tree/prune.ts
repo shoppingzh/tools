e2fe042eaace03a8258e9e34781758f6ae48859c
shoppingzh <xpzheng6@163.com>
2024-09-25 18:32:22 +0800

    feat(tree): prune docs

--- src/tree/prune.ts
+++ src/tree/prune.ts
@@ -3,6 +3,8 @@ import { BaseNode, checkNodes, DEFAULT_CHILDREN_PROP, NodePredicate } from './_b
 /**
  * 剪枝
  * 
+ * 该剪枝算法会就地(in place)修改节点的children属性
+ * 
  * @param nodes 节点列表
  * @param predicate 推断回调，返回true表示被剪掉
  * @param childrenProp 子节点列表属性
